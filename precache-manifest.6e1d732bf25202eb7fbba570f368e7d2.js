02a0c3fba5f7faf261ddbca146dd0783a48fdadc
Ivan Shevchenko <rayserviz@gmail.com>
2020-03-19 23:40:43 +0200

    Updates

---
+++ precache-manifest.6e1d732bf25202eb7fbba570f368e7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85577710278126ed899be1d657b09cb9",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "3d3d91d2e79640e47ad0",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.897e9cce.chunk.css"
+  },
+  {
+    "revision": "f4f4bb22caa2566322fd",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.93d61686.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.93d61686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3d91d2e79640e47ad0",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.12297fc6.chunk.js"
+  },
+  {
+    "revision": "8f0665075ccd8d01d4b6",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b949432e.js"
+  }
+]);
